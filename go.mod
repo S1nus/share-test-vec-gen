7e784ce37e8b2d811c9a99ba87aee5c62c607b62
c-node <chuckhead7@gmail.com>
2024-08-02 01:10:04 -0400

    start making test vectors

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/S1nus/share-test-vec-gen
 
-go 1.22.0
+go 1.22.5
+
+require (
+	github.com/celestiaorg/go-square/v2 v2.0.0-rc1 // indirect
+	golang.org/x/exp v0.0.0-20231206192017-f3f8817b8deb // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+
+	sh "github.com/celestiaorg/go-square/v2/share"
 )
 
 func main() {
-  fmt.Println("Hello world")
+	zeroes472 := make([]byte, 472)
+	for i := range zeroes472 {
+		zeroes472[i] = 0
+	}
+	namespace, _ := sh.NewNamespace(0, []byte{1, 2, 3, 4, 5})
+	blob, _ := sh.NewBlob(namespace, zeroes472, 0, nil)
+	shares, _ := splitBlobs(blob)
+	fmt.Println(shares)
+}
+
+// splitBlobs splits the provided blobs into shares.
+func splitBlobs(blobs ...*sh.Blob) ([]sh.Share, error) {
+	writer := sh.NewSparseShareSplitter()
+	for _, blob := range blobs {
+		if err := writer.Write(blob); err != nil {
+			return nil, err
+		}
+	}
+	return writer.Export(), nil
 }
