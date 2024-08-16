ed3221f9c78dbd1f8ed174f9ab23118e9d1a14f5
c-node <chuckhead7@gmail.com>
2024-08-16 17:56:35 -0400

    SubtreeWidth and MerkleMountainRangeSizes appear to behave the same as solidity. todo: automate test

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.5
 
 require (
 	github.com/celestiaorg/go-square/v2 v2.0.0-rc1 // indirect
+	github.com/celestiaorg/nmt v0.22.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	golang.org/x/exp v0.0.0-20231206192017-f3f8817b8deb // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"os"
 
+	inc "github.com/celestiaorg/go-square/v2/inclusion"
 	sh "github.com/celestiaorg/go-square/v2/share"
 )
 
@@ -26,140 +28,42 @@ type testVector struct {
 }
 
 func main() {
-	// Create a random namespace
-	randomNamespaceId := make([]byte, 28)
-	for i := 0; i < 18; i++ {
-		randomNamespaceId[i] = 0
-	}
-	rand.Read(randomNamespaceId[18:])
-	fmt.Println("Namespace ID:", randomNamespaceId)
-	randomNamespace := sh.MustNewNamespace(0, randomNamespaceId)
-
-	// Lowest application namespace
-	// Namespaces below this value are not valid for application usage
-	// 0x0000000000000000000000000000000000000000000000000000000100
-	lowestNsBytes, err := hex.DecodeString("00000000000000000000000000000000000000000000000000000100")
+	jsonFile, err := os.Open("testVectors.json")
 	if err != nil {
-		fmt.Println("Error decoding hex string:", err)
-		return
+		log.Fatalf("Failed to open file: %s", err)
 	}
-	lowestNamespace := sh.MustNewNamespace(0, lowestNsBytes)
-	fmt.Println("Zero Namespace:", lowestNamespace)
-
-	vecs := []testVector{}
-
-	for _, n := range []sh.Namespace{randomNamespace, lowestNamespace} {
-		// We choose lengths 478 and 479 because that is the boundary between needing 1 or 2 shares.
-		zeroes478 := make([]byte, 478)
-		for i := range zeroes478 {
-			zeroes478[i] = 0
-		}
-		blob, _ := sh.NewBlob(n, zeroes478, 0, nil)
-		shares, _ := splitBlobs(blob)
-		out := ""
-		for _, share := range shares {
-			out = out + fmt.Sprintf("%x", share.ToBytes())
-		}
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", zeroes478),
-			Shares:    toHexString(shares),
-		})
-
-		zeroes479 := make([]byte, 479)
-		for i := range zeroes479 {
-			zeroes479[i] = 0
-		}
-		blob2, _ := sh.NewBlob(n, zeroes479, 0, nil)
-		shares2, _ := splitBlobs(blob2)
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", zeroes479),
-			Shares:    toHexString(shares2),
-		})
-
-		// We try prefix and suffix 1 values, to catch any weirdness that might occur at the boundary.
-		prefix1_478 := make([]byte, 478)
-		prefix1_478[0] = 1
-		for i := range prefix1_478[1:] {
-			prefix1_478[i] = 0
-		}
-		blob3, _ := sh.NewBlob(n, prefix1_478, 0, nil)
-		shares3, _ := splitBlobs(blob3)
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", prefix1_478),
-			Shares:    toHexString(shares3),
-		})
-
-		suffix1_478 := make([]byte, 478)
-		for i := range suffix1_478[:477] {
-			suffix1_478[i] = 0
-		}
-		suffix1_478[477] = 1
-		blob4, _ := sh.NewBlob(n, suffix1_478, 0, nil)
-		shares4, _ := splitBlobs(blob4)
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", suffix1_478),
-			Shares:    toHexString(shares4),
-		})
-
-		suffix1_479 := make([]byte, 479)
-		for i := range suffix1_479[:478] {
-			suffix1_479[i] = 0
-		}
-		suffix1_479[478] = 1
-		blob5, _ := sh.NewBlob(n, suffix1_479, 0, nil)
-		shares5, _ := splitBlobs(blob5)
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", suffix1_479),
-			Shares:    toHexString(shares5),
-		})
-
-		prefix1_479 := make([]byte, 479)
-		prefix1_479[0] = 1
-		for i := range prefix1_479[1:] {
-			prefix1_479[i] = 0
-		}
-		blob6, _ := sh.NewBlob(n, prefix1_479, 0, nil)
-		shares6, _ := splitBlobs(blob6)
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", prefix1_479),
-			Shares:    toHexString(shares6),
-		})
-
-		randomKilobyte := make([]byte, 1024)
-		rand.Read(randomKilobyte)
-		blob7, _ := sh.NewBlob(n, randomKilobyte, 0, nil)
-		shares7, _ := splitBlobs(blob7)
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", randomKilobyte),
-			Shares:    toHexString(shares7),
-		})
-
-		random10kb := make([]byte, 1024*10)
-		rand.Read(random10kb)
-		blob8, _ := sh.NewBlob(n, random10kb, 0, nil)
-		shares8, _ := splitBlobs(blob8)
-		vecs = append(vecs, testVector{
-			Namespace: fmt.Sprintf("%x", n.Bytes()),
-			Data:      fmt.Sprintf("%x", random10kb),
-			Shares:    toHexString(shares8),
-		})
-
-	}
-
-	jsonData, _ := json.Marshal(vecs)
-	file, _ := os.Create("testVectors.json")
-	defer file.Close()
-	_, err = file.Write(jsonData)
+	defer jsonFile.Close()
+	byteValue, err := io.ReadAll(jsonFile)
 	if err != nil {
-		fmt.Println("Error writing to file:", err)
-		return
+		log.Fatalf("Failed to read file: %s", err)
+	}
+
+	var testVectors []testVector
+	err = json.Unmarshal(byteValue, &testVectors)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal json: %s", err)
+	}
+	for i, testVector := range testVectors {
+		//namespaceBytes, err := hex.DecodeString(testVector.Namespace)
+		//if err != nil {
+		//	log.Fatalf("Failed to decode namespace: %s", err)
+		//}
+		//namespace := sh.MustNewV0Namespace(namespaceBytes)
+		fmt.Println("vec :", i)
+		sharesData, err := hex.DecodeString(testVector.Shares)
+		if err != nil {
+			log.Fatalf("Failed to decode shares: %s", err)
+		}
+		subtreeWidth := inc.SubTreeWidth(len(sharesData)/512, 64)
+		//fmt.Println("Subtree width: ", subtreeWidth)
+		treeSizes, err := inc.MerkleMountainRangeSizes(uint64(len(sharesData)/512), uint64(subtreeWidth))
+		if err != nil {
+			log.Fatalf("Failed to get tree sizes: %s", err)
+		}
+		fmt.Println("len tree sizes: ", len(treeSizes))
+		for i, treeSize := range treeSizes {
+			fmt.Println("Tree size: ", i, " size: ", treeSize)
+		}
 	}
 
 }
