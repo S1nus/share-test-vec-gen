58f108b990d38bd727d82742492c1bfeb079927b
c-node <chuckhead7@gmail.com>
2024-08-20 18:37:44 -0400

    stepping through it to find divergence

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.22.5
 require (
 	github.com/celestiaorg/go-square/v2 v2.0.0-rc1 // indirect
 	github.com/celestiaorg/nmt v0.22.0 // indirect
+	github.com/cometbft/cometbft v0.38.11 // indirect
+	github.com/cosmos/gogoproto v1.4.11 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	golang.org/x/exp v0.0.0-20231206192017-f3f8817b8deb // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -10,6 +11,8 @@ import (
 
 	inc "github.com/celestiaorg/go-square/v2/inclusion"
 	sh "github.com/celestiaorg/go-square/v2/share"
+	"github.com/celestiaorg/nmt"
+	"github.com/cometbft/cometbft/crypto/merkle"
 )
 
 /*
@@ -22,9 +25,10 @@ import (
 */
 
 type testVector struct {
-	Namespace string `json:"namespace"`
-	Data      string `json:"data"`
-	Shares    string `json:"shares"`
+	Namespace  string `json:"namespace"`
+	Data       string `json:"data"`
+	Shares     string `json:"shares"`
+	Commitment string `json:"commitment"`
 }
 
 func main() {
@@ -43,27 +47,85 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to unmarshal json: %s", err)
 	}
-	for i, testVector := range testVectors {
-		//namespaceBytes, err := hex.DecodeString(testVector.Namespace)
-		//if err != nil {
-		//	log.Fatalf("Failed to decode namespace: %s", err)
-		//}
-		//namespace := sh.MustNewV0Namespace(namespaceBytes)
-		fmt.Println("vec :", i)
-		sharesData, err := hex.DecodeString(testVector.Shares)
+	for i, testVector := range testVectors[:1] {
+
+		namespaceBytes, err := hex.DecodeString(testVector.Namespace)
 		if err != nil {
-			log.Fatalf("Failed to decode shares: %s", err)
+			log.Fatalf("Failed to decode namespace: %s", err)
 		}
-		subtreeWidth := inc.SubTreeWidth(len(sharesData)/512, 64)
-		//fmt.Println("Subtree width: ", subtreeWidth)
-		treeSizes, err := inc.MerkleMountainRangeSizes(uint64(len(sharesData)/512), uint64(subtreeWidth))
+
+		namespace, err := sh.NewNamespaceFromBytes(namespaceBytes)
 		if err != nil {
-			log.Fatalf("Failed to get tree sizes: %s", err)
+			log.Fatalf("Failed to create namespace: %s", err)
 		}
-		fmt.Println("len tree sizes: ", len(treeSizes))
+
+		dataBytes, err := hex.DecodeString(testVector.Data)
+		if err != nil {
+			log.Fatalf("Failed to decode data: %s", err)
+		}
+
+		blob, err := sh.NewBlob(namespace, dataBytes, 0, nil)
+		if err != nil {
+			log.Fatalf("Failed to create blob: %s", err)
+		}
+
+		shares, err := splitBlobs(blob)
+		if err != nil {
+			log.Fatalf("Failed to split blobs: %s", err)
+		}
+		if toHexString(shares) != testVector.Shares {
+			fmt.Println("vec :", i, " shares do not match")
+		} else {
+			fmt.Println("vec :", i)
+			fmt.Println("commitment: ", testVector.Commitment)
+		}
+
+		// defaultSubtreeRootThreshold is 64
+		subtreeWidth := inc.SubTreeWidth(len(shares), 64)
+		treeSizes, err := inc.MerkleMountainRangeSizes(uint64(len(shares)), uint64(subtreeWidth))
+		if err != nil {
+			log.Fatalf("Failed to get merkle mountain range sizes: %s", err)
+		}
+		leafSets := make([][][]byte, len(treeSizes))
+		cursor := uint64(0)
 		for i, treeSize := range treeSizes {
-			fmt.Println("Tree size: ", i, " size: ", treeSize)
+			leafSets[i] = sh.ToBytes(shares[cursor : cursor+treeSize])
+			cursor += treeSize
 		}
+
+		// create the commitments by pushing each leaf set onto an NMT
+		subTreeRoots := make([][]byte, len(leafSets))
+		for i, set := range leafSets {
+			// Create the NMT. TODO: use NMT wrapper.
+			tree := nmt.New(sha256.New(), nmt.NamespaceIDSize(sh.NamespaceSize), nmt.IgnoreMaxNamespace(true))
+			for _, leaf := range set {
+				// the namespace must be added again here even though it is already
+				// included in the leaf to ensure that the hash will match that of
+				// the NMT wrapper (pkg/wrapper). Each namespace is added to keep
+				// the namespace in the share, and therefore the parity data, while
+				// also allowing for the manual addition of the parity namespace to
+				// the parity data.
+				nsLeaf := make([]byte, 0)
+				nsLeaf = append(nsLeaf, namespace.Bytes()...)
+				nsLeaf = append(nsLeaf, leaf...)
+
+				err = tree.Push(nsLeaf)
+				if err != nil {
+					log.Fatalf("Failed to push leaf: %s", err)
+				}
+			}
+			// add the root
+			root, err := tree.Root()
+			fmt.Println("the root: ", hex.EncodeToString(root))
+			if err != nil {
+				log.Fatalf("Failed to get root: %s", err)
+			}
+			subTreeRoots[i] = root
+		}
+
+		commitment := merkle.HashFromByteSlices(subTreeRoots)
+		fmt.Println("got commitment: ", hex.EncodeToString(commitment))
+		fmt.Println("expected commitment: ", testVector.Commitment)
 	}
 
 }
